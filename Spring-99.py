db6735d2f4065e99e0dc5255e24b7c7db27ff4e6
Java Software <pj.midgard@yahoo.com>
2022-05-01 14:22:21 +0100

    Add files via upload

--- Spring-99.py
+++ Spring-99.py
@@ -264,8 +264,24 @@ class compression:
                                                     if check_numner_equal==Find_center_info2:
                                                             compress_or_not_compress=2
                                                             
-                                                            
+                                           Find_Save_binary=bin(Find_Save)[2:]
+                                           lenf=len(Find_Save_binary)
+
+                                           add_bits8=""
+                                           count_bits=5-lenf%5
+                                           z=0
+                                           if count_bits!=0:
+                                                   if count_bits!=5:
+                                                        while z<count_bits:
+                                                                add_bits8="0"+add_bits8
+                                                                z=z+1
+
                                                     
+                                           Find_center_info3=add_bits8+Find_Save_binary
+                                           check_numner_equal=add_bits8+Find_Save_binary
+                                           Find_Save4=1                   
+                                           if check_numner_equal==Find_center_info2:
+                                                            compress_or_not_compress=2          
                                                     
                                                     
                                            Find_Save=Find_Save+1
