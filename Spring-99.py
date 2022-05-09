35ba95c0e22e4aa61ae18b1773333dde02a36b3d
Java Software <pj.midgard@yahoo.com>
2022-05-09 09:01:53 +0100

    Update Spring-99.py

--- Spring-99.py
+++ Spring-99.py
@@ -270,30 +270,34 @@ class compression:
                                                     
                                                     Number_divide_stop=1
                                                     Number_divide_top=1
+                                                    Divide_two3=Divide_two3+"1"    
                                                     
                                             elif Number_of_the_file==0 and Number_divide_stop==0 and bits_long==2:
                                                     
                                                     Number_divide_stop=1
                                                     Divide_two2=Divide_two2+"00"#0000, 0001, 0010, 0011; 00
                                                     Number_divide_top=0
+                                                    Divide_two3=Divide_two3+"0"
 
                                             elif T1==0 and Number_divide_stop==0 and bits_long==2:#2
                                                     Number_divide_stop=1   
                                                     Divide_two2=Divide_two2+"1"#100, 101, 110, 111
                                                     Number_divide_top=0
+                                                    Divide_two3=Divide_two3+"0"    
                                                     
                                             elif T1!=0 and Number_divide_stop==0  and bits_long==2: #1
                                                     Number_divide_stop=1
                                                     Divide_two2=Divide_two2+"01"# 0100, 0101, 0110, 0111
                                                     Number_divide_top=0
+                                                    Divide_two3=Divide_two3+"0"    
                                                     
                                             elif Number_divide_stop==1 and bits_long==2:
 
                                                      if Number_divide_top==1:
 
-                                                             Divide_two3=Divide_two3+"1"+Divide_two1
+                                                             Divide_two3=Divide_two3+Divide_two1
                                                      if Number_divide_top==0:
-                                                             Divide_two3=Divide_two3+"0"+Divide_two1
+                                                             Divide_two3=Divide_two3+Divide_two1
                                                      Number_divide_top=0
                                                              
                                                      Block_big_compress_again=Block_big_compress_again+1
