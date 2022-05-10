9bfe3ceabce20129f105243dbf0c6d8ee9611f42
Java Software <pj.midgard@yahoo.com>
2022-05-10 17:57:20 +0100

    Update Spring-99.py

--- Spring-99.py
+++ Spring-99.py
@@ -409,7 +409,7 @@ class compression:
                                     if C==1:
                                         if   Circle_times2==0:
                                                 
-
+                                                SpinS=0
                                                 Translate_info_Decimal=Equal_info_between_of_the_cirlce_of_the_file[0:8]
                                                 Translate_info_Decimal_2 = int(Translate_info_Decimal, 2)
                                                 if Translate_info_Decimal_2>7:
